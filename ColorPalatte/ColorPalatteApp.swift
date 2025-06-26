fe1c6bdfef67e1b1453b05ca606662707abfe487
hxycindyhxy <xinyi.hu-3@student.uts.edu.au>
2025-06-26 12:18:44 +1000

    Initial Commit

---
+++ ColorPalatte/ColorPalatteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPalatteApp.swift
+//  ColorPalatte
+//
+//  Created by Xinyi Hu on 26/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct ColorPalatteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorPalatteTests/ColorPalatteTests.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPalatteTests.swift
+//  ColorPalatteTests
+//
+//  Created by Xinyi Hu on 26/6/2025.
+//
+
+import Testing
+@testable import ColorPalatte
+
+struct ColorPalatteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
